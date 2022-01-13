b2928ee93d865b9a3fafaca9d73564cf102f86a7
Mathieu Piton <mathieu.piton@credit-municipal-services.fr>
2022-01-13 21:13:12 +0100

    Add morgan with winston

--- src/app.ts
+++ src/app.ts
@@ -3,15 +3,15 @@ import * as errorHandler from "@/middlewares/errorHandler";
 import cors from "cors";
 import express from "express";
 import helmet from "helmet";
-import morgan from "morgan";
 import routes from "@/routes";
+import morganMiddleware from "./middlewares/morganMiddleware";
 
 export const createApp = (): express.Application => {
 	const app = express();
 
 	app.use(cors());
 	app.use(helmet());
-	app.use(morgan("dev"));
+	app.use(morganMiddleware);
 	app.use(express.json());
 	app.use(
 		express.urlencoded({
