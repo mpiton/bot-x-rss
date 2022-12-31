c7ea2654114844450311270ea0b0069e36444f24
Mathieu Piton <6437144-mpiton@users.noreply.gitlab.com>
2022-12-31 09:19:12 +0100

    Modification de la date d'expiration de la collection à 7 jours

--- src/models/Tweet.ts
+++ src/models/Tweet.ts
@@ -1,16 +1,16 @@
-import mongoose from "mongoose";
 import { TweetDocument } from "@/types/tweet/ITweet";
+import mongoose from "mongoose";
 
 const TweetSchema = new mongoose.Schema(
-	{
-		_id: { type: String },
-		link: { type: String, required: true },
-		pubDate: { type: String, required: true },
-		title: { type: String, required: true },
-		sended: { type: Boolean, required: true, default: false },
-		expire_at: { type: Date, default: Date.now, expires: 7200 }, // delete this document after 2h
-	},
-	{ timestamps: true }
+  {
+    _id: { type: String },
+    link: { type: String, required: true },
+    pubDate: { type: String, required: true },
+    title: { type: String, required: true },
+    sended: { type: Boolean, required: true, default: false },
+    expire_at: { type: Date, default: Date.now, expires: 604800 }, // delete this document after 7 jours
+  },
+  { timestamps: true }
 );
 
 const Tweet = mongoose.model<TweetDocument>("Tweet", TweetSchema);
